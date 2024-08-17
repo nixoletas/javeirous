2da974a7696d41408e1a314c199c00ce5a8e19b5
Nicholas_Miyasato <nicholetas@gmail.com>
2024-08-16 22:06:23 -0400

    feat: validation on MilitarEntity

--- src/main/java/com/nick/javeurios/controller/MilitarController.java
+++ src/main/java/com/nick/javeurios/controller/MilitarController.java
@@ -4,13 +4,17 @@ import com.nick.javeurios.entity.CargaEntity;
 import com.nick.javeurios.entity.MilitarEntity;
 import com.nick.javeurios.repository.CargaRepository;
 import com.nick.javeurios.repository.MilitarRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-
+@Validated
 @RestController
 @RequestMapping("/api/militares")
 public class MilitarController {
@@ -24,12 +28,12 @@ public class MilitarController {
     }
 
     @PostMapping
-    public MilitarEntity create(@RequestBody MilitarEntity militarEntity){
+    public MilitarEntity create(@RequestBody @Valid MilitarEntity militarEntity){
         return militarRepository.save(militarEntity);
     }
 
     @PutMapping("/{id}")
-    public MilitarEntity update(@PathVariable("id") Long id, @RequestBody MilitarEntity militarEntity) {
+    public MilitarEntity update(@PathVariable("id") Long id, @RequestBody @NotNull @Positive MilitarEntity militarEntity) {
         if (militarRepository.existsById(id)) {
             militarEntity.setId(id);
             return militarRepository.save(militarEntity);
@@ -40,7 +44,7 @@ public class MilitarController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable("id") Long id) {
+    public void delete(@PathVariable("id") @NotNull @Positive Long id) {
         if (militarRepository.existsById(id)) {
             militarRepository.deleteById(id);
         } else {
--- src/main/java/com/nick/javeurios/entity/MilitarEntity.java
+++ src/main/java/com/nick/javeurios/entity/MilitarEntity.java
@@ -1,26 +1,37 @@
 package com.nick.javeurios.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Table(name = "tb_militar")
 public class MilitarEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @Length(min = 10, max = 10)
     private String identidade;
+
+    @NotNull
+    @Pattern(regexp = "Cel|TC|Maj|Cap|1º Ten|2º Ten|Asp Of|ST|1º Sgt|2º Sgt|3º Sgt|Cb|Sd EP| Sd EV")
     private String pg;
+
+    @NotNull
+    @Length(max = 30)
     private String nome;
+
+    @Pattern(regexp = "Cia Com|Cia GE|COGE|Cia C2|CCAp|EM|Almox|Saude|Garagem|Res Armto")
     private String subunidade;
+
     private String telefone;
 
 }
